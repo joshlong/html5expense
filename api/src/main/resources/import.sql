0282811f0f1cf83f7e9781e046cfa906396d918e
Josh Long <josh@joshlong.com>
2011-12-06 01:40:34 -0800

    ading the css

---
+++ api/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into eligible_charge( amount, category ,date, merchant ) values( 242.24,  'travel', now() , 'southwest' );
+insert into eligible_charge( amount, category ,date, merchant ) values( 23.5,  'food', now() , 'starbucks' );
+insert into eligible_charge( amount, category ,date, merchant ) values( 250.0,  'travel', now() , 'united airlines' );
+insert into eligible_charge( amount, category ,date, merchant ) values( 4500.0,  'computer supplies', now() , 'apple computer' );
+insert into eligible_charge( amount, category ,date, merchant ) values( 150.0,  'travel', now() , 'american airlines' );
+insert into eligible_charge( amount, category ,date, merchant ) values( 15.5,  'personal', now() , 'ghostbusters' );
